85e43287cefcb1f3e39663826ac7d208bba3c822
AhnDongwon <lemontech119@gmail.com>
2019-10-16 16:20:09 +0900

    model 수정

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,14 +1,15 @@
 class Book {
-    constructor(id, title, author, date){
+    constructor(id, title, author, rentalYn, date){
         this.id = id;
         this.title = title;
         this.author = author;
+        this.rentalYn = rentalYn;
         this.date = date;
     }
 
-    static newInstance(id, title, author){
+    static newInstance(id, title, author, rentalYn){
         const date = `${Date.now()}`;
-        return new Book(id, title, author, date);
+        return new Book(id, title, author, rentalYn, date);
     }
 
     toJSON(){
@@ -16,12 +17,13 @@ class Book {
             id: this.id,
             title: this.title,
             author: this.author,
+            rentalYn: this.rentalYn,
             date: this.date
         }
     }
 
     static fromData(data){
-        return new Book(data.id, data.title, data.author, data.date);
+        return new Book(data.id, data.title, data.author, data.rentalYn, data.date);
     }
 }
 
